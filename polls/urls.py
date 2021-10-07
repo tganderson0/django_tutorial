fe3c7e72a7a465ec1f0c2b6d951f52f2cc2ce799
gaseoustortoise <tganderson0@gmail.com>
2021-10-07 12:23:28 -0600

    Finished part 3, (last one was part 2 I just have fat fingers haha)

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    # EX: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # EX: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # EX: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world, you're at the polls index")
+    # Get the information from the database
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+   
+    
+    # The dictionary values used in the template
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+
+    # Render the webpage and return it
+    return HttpResponse(render(request, 'polls/index.html', context))
+
+def detail(request, question_id):
+
+    # Looks for the specified object, gets a 404 if it does not exist
+    question = get_object_or_404(Question, pk=question_id)
+
+    # If you want a list rather than object use:
+    # get_list_or_404()
+
+    # Dictionary for render
+    context = {
+        'question': question,
+    }
+    return HttpResponse(render(request, 'polls/detail.html', context))
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
